9dc99ab5b8128e23142cf9d9b77530207f234e9a
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-09-13 19:54:30 -0500

    - Added: description of the test of the Auction model

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes"
+  it "it not valid without a title"
+  it "is not valid without a description"
+  it "is not valid without a start_date"
+  it "is not valid without a end_date"
 end
