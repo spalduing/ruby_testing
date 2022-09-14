628de76d0cdff45afa2a543a3df6468733a7640c
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-09-13 22:51:28 -0500

    - Added: Bid concept, bussines behaviour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_032913) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_14_034929) do
   create_table "auctions", force: :cascade do |t|
     t.datetime "start_date"
     t.datetime "end_date"
@@ -19,6 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_14_032913) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "bids", force: :cascade do |t|
+    t.integer "bidder_id"
+    t.integer "auction_id"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
