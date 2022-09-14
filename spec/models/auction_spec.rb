220d292ea42cf555a4357ece411c6df6230dd77d
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-09-13 21:20:08 -0500

    - Added: validations

--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,10 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Auction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-  it "is valid with valid attributes"
-  it "it not valid without a title"
-  it "is not valid without a description"
-  it "is not valid without a start_date"
-  it "is not valid without a end_date"
+  # pending 'add some examples to (or delete) #{__FILE__}'
+  subject { described_class.new(title: 'Anything',
+                                description: 'Anything',
+                                start_date: DateTime.now,
+                                end_date: DateTime.now + 1.week,)
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a start_date' do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a end_date' do
+    subject.end_date = nil
+    expect(subject).to_not be_valid
+  end
 end
